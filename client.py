71ce6b2a7d06098b1ae536b56ebfca8b059022cd
Rahul Gautam <RGautam@iaai.com>
2020-03-11 15:43:00 -0500

    added medium url for reference

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#Refrence from the following link
+#https://towardsdatascience.com/create-an-api-to-deploy-machine-learning-models-using-flask-and-heroku-67a011800c50
 import json
 import requests
 
